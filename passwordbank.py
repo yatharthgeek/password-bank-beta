b3a6ae9d602eeef865c2675da15788daed69a802
yatharthgeek <81706493+yatharthgeek@users.noreply.github.com>
2022-03-19 00:03:24 -0700

    Add files via upload

--- passwordbank.py
+++ passwordbank.py
@@ -78,13 +78,133 @@ def new():
             file = open(filename , "w+")
             file.write(holder_unique_id)
 
+            login_req = input("\nDo you want to login ? (Y/N)\n")
+            if login_req == "Y" or login_req == "y" :
+                existing()
+
+            elif login_req == "N" or login_req == "n" :
+                break
+
+
+
+def existing():
+    requested_acc_id = input("\nEnter Your Account ID \n")
+    requested_acc_pwd = input("\nEnter Your Password \n")
+    requested_uid = input("\nAccount Unique Id file Path \n(eg. /file/acc_uid001122.txt)\n")
+
+    save_file = open(requested_uid , "r")
+    retrived_uid = save_file.read()
+
+    r= firebase.get("/password-database-df4db-default-rtdb/Account",retrived_uid)
+    retrived_acc_id = r.get("Account ID")
+    retrived_pwd = r.get("Password")
+    retrived_name = r.get("Name")
+
+    if retrived_acc_id == requested_acc_id and retrived_pwd == requested_acc_pwd :
+        print("\nWelcome ",retrived_name)
+        submenu()
+
+        while True :
+
+            bash_log = int(input("\nEnter Your Choice : \n"))
+
+            if bash_log == 4:
+                new_passwd()
+
+            elif bash_log == 5 :
+                ret_pwd()
+
+            elif bash_log == 6 :
+                ddel_rec()
+
+            else :
+                print("Unknow Input")
+
+        
+
+    else :
+        print("Sorry , Bad request . ")
+
+def submenu():
+
+    #Menu when account is logged in 
+    print("\n[4] Save New Password\n[5] Retrieve all Passwords\n[6] Delete Records\n[7]Logout")
+
+def new_passwd():
+
+    app_name = input("\nApp Name\n")
+    username_app = input("\nEnter Username\n")
+    passwd_app =input("\nEnter Password\n")
+
+    upload_pwd = {
+                "App Name":app_name,
+                "Username":username_app,
+                "Password":passwd_app,
+
+            }
+    
+    pwd_result = firebase.post("/password-database-df4db-default-rtdb/Passwords" , upload_pwd)
+            
+    print("\n Your Password Unique ID is (Always Remember it ) :")
+    pwd_unique_id = pwd_result.get("name")
+
+    print("\nData Sucessfully Updated\n")
+
+    filename= "pwduid"+app_name+".txt"
+
+    file = open(filename , "w+")
+    file.write(pwd_unique_id)
+
+def ret_pwd():
+    requested_uid_pwd = input("\nAccount Unique Id file Path \n(eg. /file/acc_uid001122.txt)\n")
+
+    save_file_pwd = open(requested_uid_pwd , "r")
+    retrived_uid_pwd = save_file_pwd.read()
+
+    re= firebase.get("/password-database-df4db-default-rtdb/Passwords",retrived_uid_pwd)
+    retrived_username = re.get("Username")
+    retrived_passwd = re.get("Password")
+
+    print("Username : ",retrived_username)
+    print("Password :",retrived_passwd)
+
+def ddel_rec():
+    print("Feature will Updated Soon ")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 information()
 menu()
-bash =int( input ("\nEnter Your Choice : \n"))
 
-if bash == 1:
-    new()
+while True :
+    
+    bash =int( input ("\nEnter Your Choice : \n"))
+
+    if bash == 1:
+        new()
+
+    elif bash == 2 :
+        existing()
+
+    elif bash == 3 :
+        print("Thanks for Using")
+
+    else :
+        print("Unknown Input")
+
+
+
+
 
